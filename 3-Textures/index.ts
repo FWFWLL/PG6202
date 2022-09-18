9de3af7bbf2c8c1c3f24b010df427f94ce2a770d
FWFWLL <ffl@vivaldi.net>
2022-09-18 12:36:38 +0200

    Added 3-Textures

---
+++ 3-Textures/index.ts
@@ -0,0 +1,35 @@
+/// <reference path="node_modules/webpack-dev-server/types/lib/Server.d.ts"/>
+import type {Configuration} from "webpack";
+
+import {ArcRotateCamera, Color4, Engine, HemisphericLight, MeshBuilder, Scene, ShaderMaterial, Texture, Vector3} from "babylonjs";
+
+const canvas: any = document.getElementById("renderCanvas");
+const engine: Engine = new Engine(canvas, true);
+
+function createScene() : Scene {
+	const scene: Scene = new Scene(engine);
+	scene.clearColor = new Color4(0.1, 0.1, 0.1, 1);
+
+	const camera: ArcRotateCamera = new ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+	camera.attachControl(canvas, true);
+
+	const light: HemisphericLight = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
+
+	const shaderMaterial = new ShaderMaterial("shader", scene, {vertexElement: "vertexShaderCode", fragmentElement: "fragmentShaderCode"}, {
+		attributes: ["position", "uv"]
+	});
+
+	let texture = new Texture("/textures/plane.basis", scene)
+	shaderMaterial.setTexture("textureSampler", texture);
+
+	const plane = MeshBuilder.CreatePlane("plane", {size: 1}, scene);
+	plane.material = shaderMaterial;
+
+	return scene;
+}
+
+const scene: Scene = createScene();
+
+engine.runRenderLoop(() => {
+	scene.render();
+});
